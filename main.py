fa07ad273453830501317576a8049bd8aa04f8ff
moughit <moughitmoubarik08@gmail.com>
2024-01-03 17:21:30 +0100

    ddss

--- main.py
+++ main.py
@@ -2,12 +2,22 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from scan import router as scan_router
+from fastapi.middleware.cors import CORSMiddleware
+
 # Create a FastAPI instance
 app = FastAPI()
 
 
 app.include_router(scan_router)
-
+# CORS middleware
+origins = ["*"]  
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Redirect the root URL to the documentation
 @app.get("/", include_in_schema=False)
